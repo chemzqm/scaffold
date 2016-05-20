471379b33681891e6eaaabca25d9676ff33c3dc4
chemzqm <chemzqm@gmail.com>
2016-05-20 22:10:42 +0800

    remove unnecessary fields

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 'use strict'
-import Emitter from 'emitter'
 
 class Mod extends Emitter {
-  x() {
-    let a = 1
-    let b = 2
-    return `${a} ${b}`
-  }
-  static y() {
-    return 111
-  }
 }
 
 export default Mod
